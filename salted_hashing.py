84bbbfa1fb27249392c5973272154d497d9cfdb0
Yang Yu <yy2023@seas.upenn.edu>
2025-07-03 01:08:01 +0000

    fix test issue

--- salted_hashing.py
+++ salted_hashing.py
@@ -4,9 +4,13 @@ import os
 def myhash(m):
     #Generate random nonce
     nonce_bytes = os.urandom(32)
+
     nonce = nonce_bytes.hex()
+
     concatenated = nonce + m
-    #Generate hex digest
+    
     sha256_hash = hashlib.sha256(concatenated.encode('utf-8'))
+
     h_hex = sha256_hash.hexdigest()
+    
     return nonce, h_hex
